b92b4d065846215fb15695a463e9a76fa71d0673
Dennis <jhbang8917@gmail.com>
2019-08-27 21:21:42 +0900

    spring boot init

---
+++ src/main/java/org/respec/respecapi/RespecApiApplication.java
@@ -0,0 +1,13 @@
+package org.respec.respecapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RespecApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RespecApiApplication.class, args);
+    }
+
+}
