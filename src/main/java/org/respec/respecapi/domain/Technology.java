e2d8eb708c88731cdd87cd3c6e14d5072229c338
oscar021004 <dudwnssl80@gmail.com>
2019-09-29 18:01:42 +0900

    Technology table name mapping

--- src/main/java/org/respec/respecapi/domain/Technology.java
+++ src/main/java/org/respec/respecapi/domain/Technology.java
@@ -3,14 +3,12 @@ package org.respec.respecapi.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name="technologies")
 @Getter
 @NoArgsConstructor
 public class Technology {
